1927b8ed68a64aca2b18c5326332d88a300963bc
boc5244 <31375763+boc5244@users.noreply.github.com>
2020-05-16 22:24:47 -0400

    Updates

---
+++ Registration/batch_filtvid.m
@@ -0,0 +1,34 @@
+function [] = batch_filtvid(root, n, Fc)
+%% batch_filtvid: low-pass filter videos in time, save to new folder
+%
+%   INPUT:
+%       vid         : video data
+%       n           : # frames to filter
+%
+%   OUTPUT:
+%       VID         : filtered video data
+%
+
+% root = 'H:\EXPERIMENTS\RIGID\Experiment_Static_Wave';
+
+[FILES, PATH] = uigetfile({'*.mat'},'Select videos', root, 'MultiSelect','on');
+FILES = string(FILES);
+nfile = length(FILES);
+
+filtdir = fullfile(PATH,'vid_filt');
+mkdir(filtdir)
+
+for file = 1:nfile
+    disp(FILES(file))
+    disp('---------------------------------------')
+    load(fullfile(PATH,FILES(file)),'vidData','t_v')
+    
+    Fs = round( 1 /mean(diff(t_v)) );
+
+    filtvid = filtfilt_vid(vidData, n, Fc, Fs);
+
+    save(fullfile(filtdir,FILES{file}), '-v7.3', 'filtvid', 't_v')
+end
+disp('ALL DONE')
+
+end
---
+++ Registration/filtfilt_vid.m
@@ -0,0 +1,41 @@
+function [filtvid] = filtfilt_vid(vid,n,Fc,Fs,debug)
+%% filtfilt_vid: applies zero-phase filter to each pixel in a greyscale video over in time
+%
+%   INPUT:
+%       vid         : video data
+%       n           : filter order
+%       Fc          : cutoff-frequency [Hz]
+%       Fs          : sampling-frequency [Hz]
+%       debug      	: show comparison
+%
+%   OUTPUT:
+%       filtvid  	: filtered video data
+%
+
+if nargin < 5
+    debug = false;
+end
+
+A = squeeze(vid);
+dim = size(A);
+B = reshape(A,[dim(1)*dim(2) dim(3)]);
+[b,a] = butter(n, Fc / (Fs/2), 'low');
+C = filtfilt(b,a,double(B)')';
+filtvid = uint8(reshape(C,dim));
+
+if debug
+    fig = figure(1) ; clf
+    set(fig, 'Color', 'w')
+    ax(1) = subplot(1,2,1); axis image
+    ax(2) = subplot(1,2,2); axis image
+    set(ax, 'Box', 'on')
+    for f = 1:dim(3)
+        subplot(1,2,1) ; cla
+            imshow(vid(:,:,f))
+        subplot(1,2,2) ; cla
+            imshow(filtvid(:,:,f))
+        pause(0.001)
+    end
+end   
+
+end
