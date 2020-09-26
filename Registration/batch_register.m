df578c4260c96648058bd877872d4bd12f2f153e
boc5244 <31375763+boc5244@users.noreply.github.com>
2020-09-25 20:01:39 -0400

    Update registration

--- Registration/batch_register.m
+++ Registration/batch_register.m
@@ -1,14 +1,17 @@
-function [] = batch_register(root)
+function [] = batch_register(root, crop_xy)
 %% batch_register: register selected videos
 %
 %   INPUT:
 %       root   	:   root directory
+%       crop    : 
 %
 %   OUTPUT:
 %       -
 %
 
-% root = 'H:\EXPERIMENTS\MAGNO\Experiment_SOS';
+if nargin < 2
+   crop_xy = []; 
+end
 
 [FILES, PATH] = uigetfile({'*.mat', 'MAT-files'},'Select videos', root, 'MultiSelect','on');
 FILES = string(FILES);
@@ -22,7 +25,13 @@ for file = 1:nfile
     disp('---------------------------------------')
     load(fullfile(PATH,FILES(file)),'vidData','t_v')
 
-    [regvid,trf] = register_video(vidData);
+    if file == 1
+        if length(crop_xy) == 1
+            [crop_frame, crop_xy] = imcrop(vidData(:,:,:,1));
+        end
+    end
+
+    [regvid,trf] = register_video(vidData, crop_xy);
 
     %save(fullfile(regdir,FILES{file}),'-v7.3','regvid','trf','t_v')
     save(fullfile(regdir,FILES{file}),'-v7.3','regvid','trf')
--- Registration/register_video.m
+++ Registration/register_video.m
@@ -1,4 +1,4 @@
-function [vid,trf] = register_video(vid)
+function [vid,trf] = register_video(vid, crop_xy)
 %% register_video: register each frame of a video to the same reference
 %
 %   INPUT:
@@ -9,6 +9,38 @@ function [vid,trf] = register_video(vid)
 %       trf   	:   2D affine transformation for each frame
 %
 
+if nargin < 2
+    crop_xy = [];
+end
+
+vid = squeeze(vid); % In case it's 4D
+dim = size(vid); % video size
+
+% Crop video
+if isempty(crop_xy)
+    crop_vid = vid;
+elseif length(crop_xy) == 2
+    x_range = round(crop_xy(1):dim(2)-crop_xy(1));
+    y_range = round(crop_xy(2):dim(1)-crop_xy(2));
+    crop_vid = vid(y_range,x_range,:);
+elseif length(crop_xy) == 4 % rectangle
+    init_frame = imcrop(vid(:,:,1), crop_xy);
+    crop_vid = uint8(zeros(size(init_frame)));
+    for n = 1:dim(3)
+       crop_vid(:,:,n) = imcrop(vid(:,:,n), crop_xy);
+    end
+else
+    error('"crop_xy" must be 2x1 or 4x1')
+end
+
+% Get intial orientation
+v1 = crop_vid(:,:,1); % initial frame
+v2 = medfilt2(imopen(v1,strel('disk',15)),[7 7]);
+v3 = imbinarize(v2);
+L = regionprops(v3,'Orientation'); % initial angle
+refangle = L.Orientation;
+% [refangle,~] = findheading(v1, false, 0);
+
 % Set up optimizer
 [optimizer, metric] = imregconfig('monomodal');
 % optimizer.InitialRadius = 1e-3;
@@ -16,42 +48,29 @@ optimizer.MaximumIterations = 150;
 optimizer.MaximumStepLength = 0.03;
 optimizer.MinimumStepLength = 0.0002;
 
-vid = flipud(squeeze(vid)); % In case it's 4D
-dim = size(vid); % Used often
-
-% Get intial orientation
-v1 = vid(:,:,1); % initial frame
-% v2 = medfilt2(imopen(v1,strel('disk',15)),[7 7]);
-% v3 = im2bw(v2,graythresh(v2));
-% v3 = imbinarize(v2);
-% L = regionprops(v3,'Orientation'); % initial angle
-% refangle = L.Orientation;
-[refangle,~] = findheading(v1, false, 0);
-
 trf     = cell(dim(3),1); % store 2D affine transformations here
-fixed   = double(squeeze(vid(:,:,1)));
+fixed   = squeeze(crop_vid(:,:,1));
 sz      = imref2d(size(fixed));
 
 % Register each frame with respect to the first frame
 tic
-for kk = 1:dim(3)
-    fprintf([int2str(kk) '\n']);
-    z = double(vid(:,:,kk)); % take one frame
-    if kk==1
-        trf{kk} = imregtform(z,fixed,'rigid',optimizer,metric);
+for n = 1:dim(3)
+    fprintf([int2str(n) '\n'])
+    z = (crop_vid(:,:,n));
+    if n == 1
+        trf{n} = imregtform(z, fixed, 'rigid', optimizer, metric);
     else
-        for jj = kk-1:-1:1
+        for jj = n-1:-1:1
             if(isRigid(trf{jj}))
                 break;
             end
         end
-        trf{kk} = imregtform(z,fixed,'rigid',optimizer,metric,...
-                            'InitialTransformation',trf{jj});
-                        %,'PyramidLevels',5);
+        trf{n} = imregtform(z, fixed, 'rigid', optimizer, metric,...
+                            'InitialTransformation', trf{jj});
     end
-    reg = imwarp(z,trf{kk},'OutputView',sz);
-    vid(:,:,kk) = imrotate(reg,90-refangle,'crop');
-    fixed = (fixed*kk + reg)/(kk+1);
+    reg = imwarp(z, trf{n}, 'OutputView', sz);
+    crop_vid(:,:,n) = imrotate(reg, 90-refangle, 'crop');
+    fixed = (fixed*n + reg)/(n+1);
 end
 disp('DONE')
 toc
