aef8b24c12bb7c13eee5f2db5b2163df73a00733
Benjamin Cellini <31375763+boc5244@users.noreply.github.com>
2020-11-09 17:59:15 -0500

    Updates

--- Arena_time_test.m
+++ Arena_time_test.m
@@ -2,34 +2,35 @@
 close all
 clc
 
-Fs = 500;
+Fs = 50;
+T = 20;
 
 [TRIG,PAT] = sync_pattern_trigger(t_p, data(:,2), 20, data(:,1), false, [], false, false);
 
 pat_time = PAT.time_sync(PAT.sync:PAT.end_idx);
 pat_pos = 3.75*PAT.pos(PAT.sync:PAT.end_idx);
-pat_pos = interp1(pat_time, pat_pos, (0:(1/Fs):10)', 'linear');
-pat_time = (0:(1/Fs):10)';
+pat_pos = interp1(pat_time, pat_pos, (0:(1/Fs):T)', 'linear');
+pat_time = (0:(1/Fs):T)';
 pat_vel = diff(pat_pos) / (1/Fs);
 
-[Fv, Mag , ~ , ~] = FFT(pat_time, pat_vel);
+[Fv, Mag , ~ , ~] = FFT(pat_time, pat_pos);
 
 subplot(2,1,1) ; hold on
-% plot(All.time, All.X_step - All.X_step(1), 'k')
-% plot(pat_time, pat_pos - pat_pos(1), 'r')
-plot(All.time, All.X_step - mean(All.X_step), 'k')
-plot(pat_time, pat_pos - mean(pat_pos), 'r')
+plot(All.time, All.X_step - All.X_step(1), 'k')
+plot(pat_time, pat_pos - pat_pos(1), 'r')
+% plot(All.time, All.X_step - mean(All.X_step), 'k')
+% plot(pat_time, pat_pos - mean(pat_pos), 'r')
 xlim([0 10])
-
-% subplot(2,1,2) ; hold on
-% plot(Fv, All.mag.dX, 'k')
-% plot(Fv, Mag, 'r')
-% xlim([0 10])
 
 subplot(2,1,2) ; hold on
-[Fv, Mag , ~ , ~] = FFT(All.time, All.X_step);
-plot(Fv, Mag, 'k')
-
-[Fv, Mag , ~ , ~] = FFT(pat_time, pat_pos);
+plot(All.Fv, All.mag.X_step, 'k')
 plot(Fv, Mag, 'r')
-xlim([0 10])
+xlim([0.1 10])
+
+% subplot(2,1,2) ; hold on
+% [Fv, Mag , ~ , ~] = FFT(All.time, All.X_step);
+% plot(Fv, Mag, 'k')
+% 
+% [Fv, Mag , ~ , ~] = FFT(pat_time, pat_pos);
+% plot(Fv, Mag, 'r')
+% xlim([0 10])
