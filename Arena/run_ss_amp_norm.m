34a7eaffa1119ad15a26250bf8b2b27f65d8b1a6
boc5244 <31375763+boc5244@users.noreply.github.com>
2020-11-30 16:23:47 -0500

    Updates

--- Arena/run_ss_amp_norm.m
+++ Arena/run_ss_amp_norm.m
@@ -21,7 +21,7 @@ clear ; close all ; clc
 
 root        = 'C:\Users\BC\Box\Git\Fly-Multibody\Arena\functions';
 F           = [];
-T           = 20;
+T           = 10;
 Fs          = 200;
 A           = 3.75*[16 11 7 5 3 2 1];
 norm_vel   	= 250;
@@ -29,13 +29,13 @@ res         = 3.75;
 cent        = 45;
 showplot    = true;
 
-n_amp = length(A);
+n_amp = length(A); 
 freq_all = nan(n_amp,1);
 for a = 1:n_amp
     All = make_sos(T, Fs, res, F, A(a), norm_vel, cent, showplot, root);
     freq_all(a) = All.Freq;
-    %pause
-    %close all
+%     pause
+%     close all
 end
 disp(freq_all)
 
