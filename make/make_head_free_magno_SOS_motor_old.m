f196d8656d31d61b35d9302985fc6878053e02a1
Benjamin Cellini <31375763+boc5244@users.noreply.github.com>
2021-12-07 17:58:07 -0500

    Updates

--- make/make_head_free_magno_SOS_motor_old.m
+++ make/make_head_free_magno_SOS_motor_old.m
@@ -10,9 +10,9 @@ function [] = make_head_free_magno_SOS_motor(rootdir)
 warning('off', 'signal:findpeaks:largeMinPeakHeight')
 
 clss = 'position';
-clss = 'velocity';
+% clss = 'velocity';
 
-rootdir = 'S:\Restricted\BC\Backup\EXPERIMENTS\MAGNO\Experiment_SOS_vel_v2_motor';
+rootdir = 'E:\EXPERIMENTS\MAGNO\Experiment_SOS_motor_vel_v2';
 exp_name = textscan(char(rootdir), '%s', 'delimiter', '_');
 exp_typ = exp_name{1}{end-1}; % type of stimuli (vel or pos)
 exp_ver = exp_name{1}{end}; % version of experiment (v1, v2, ...)
@@ -23,9 +23,8 @@ root.base = rootdir;
 root.body = fullfile(root.base,'tracked_body');
 root.reg = fullfile(root.base,'registered');
 root.benifly = fullfile(root.reg ,'tracked_head_wing');
-root.head = fullfile(root.reg ,'tracked_head');
+root.head = fullfile(root.reg ,'tracked_head_tip');
 root.func = fullfile(root.base ,'function');
-root.replay = fullfile(root.base ,'replay');
 
 % Load function files
 func_list = dir(root.func);
@@ -37,100 +36,109 @@ for f = 1:n_cond
     FUNC{f}.name = func_list(f).name;
 end
 
-% Load replay file
-func_list = dir(root.replay);
-func_list = func_list(~[func_list.isdir]);
-n_cond = length(func_list); % number of stimuli (functions) used in experiment
-Replay = cell(n_cond,1);
-for f = 1:n_cond
-    Replay{f} = load(fullfile(root.replay, func_list(f).name));
-    Replay{f}.name = func_list(f).name;
-end
-Replay = cellfun(@(x) x.replay, Replay, 'UniformOutput', false);
-
 % Select files
+% [D,I,N,U,T,~,~,basename] = GetFileData(root.head,'*.mat',false);
 [D,I,N,U,T,~,~,basename] = GetFileData(root.head,'*.mat',false);
-% [D,I,N,U,T,~,~,basename] = GetFileData(root.body,'*.mat',false);
 % [D,I,N,U,T,~,~,basename] = GetFileData(root.benifly,'*.csv',false);
 
 %% Get Data
 close all
 clc
 
+% Body saccade detection parameters
+scd.threshold = [20, 1, 3, 0];
+scd.true_thresh = 220;
+scd.Fc_detect = [40 nan];
+scd.Fc_ss = [20 nan];
+scd.amp_cut = 5;
+scd.dur_cut = 1;
+scd.direction = 0;
+scd.direction = 0;
+scd.pks = [];
+scd.sacd_length = nan;
+scd.min_pkdist = 0.2;
+scd.min_pkwidth = 0.02;
+scd.min_pkprom = 50;
+scd.min_pkthresh = 0;
+scd.boundThresh = [0.2 40];
+
 Fs = 100;
 Fc = 40;
 func_length = 20;
 tintrp = (0:(1/Fs):func_length)';
 debug = false;
+[b,a] = butter(3, Fc/(Fs/2),'low');
 ALL = cell(N.fly,N{1,3});
 DATA = [D , splitvars(table(num2cell(zeros(N.file,8))))];
 DATA.Properties.VariableNames(4:end) = {'reference','body','head','error',...
     'dwba','lwing','rwing','body_saccade'};
-Body_All = [];
-Body_Freq = [];
 for n = 1:N.file
     %disp(kk)
     disp(basename{n})
     % Load DAQ, body, head, & wing data
 	data.daq = load(fullfile(root.base,  [basename{n} '.mat']),'data','t_p'); % load camera trigger & pattern x-position
-    data.body = load(fullfile(root.body, [basename{n} '.mat']),'bAngles','t_v'); % load body angles
-    data.head = load(fullfile(root.head, [basename{n} '.mat']),'hAngles'); % load head angles
-    % data.benifly = ImportBenifly(fullfile(root.benifly, ...  % load head & wing angles from Benifly
-    %                         [basename{n} '.csv']));
-    
-    % Get start point
-    head = data.head.hAngles;
-    body = data.body.bAngles;
-    [first_peak] = find_first_peak(body, data.body.t_v, 20, 20, false);
-    
-    % Put in DAQ reference frame
-    daq_time = data.daq.t_p;
-    trigger = round(data.daq.data(:,1));
-    [~,locs] = findpeaks(trigger, 'MinpeakHeight', 1);
-    trig_time_raw = daq_time(locs);
-    [~,first_peak_daq] = min(abs(trig_time_raw(first_peak) - daq_time));
+    data.body = load(fullfile(root.body, [basename{n} '.mat']),'bAngles'); % load body angles
+	data.head = load(fullfile(root.head, [basename{n} '.mat']),'head_data'); % load head angles
+ 	data.benifly = ImportBenifly(fullfile(root.benifly, ...  % load head & wing angles from Benifly
+                            [basename{n} '.csv']));
     
     % Get synced frame times and pattern data
+    trigger = data.daq.data(:,1);
+    daq_time = data.daq.t_p;
     daq_pattern = data.daq.data(:,2);
-    [TRIG,PAT]  = sync_pattern_trigger(daq_time, daq_pattern, func_length, ...
-                        trigger, true, first_peak_daq - 900, false, false);
-
-    trig_time = TRIG.time_sync(1:end);
+    if std(daq_pattern) < 0.01 % no pattern mvoement
+        startI = 2500;
+        [TRIG,PAT]  = sync_pattern_trigger(daq_time, daq_pattern, func_length, ...
+                            trigger, true, startI, false, false);
+    else
+        [TRIG,PAT]  = sync_pattern_trigger(daq_time, daq_pattern, func_length, ...
+                            trigger, true, [], false, false);
+    end
+    trig_time = TRIG.time_sync(1:end-1);
     
-    % % Filter wing angles
-    % lwing = rad2deg(data.benifly.LWing);
-    % rwing = rad2deg(data.benifly.RWing);
-    % lwing = hampel(data.benifly.Time, lwing);
-    % rwing = hampel(data.benifly.Time, rwing);
-    % lwing = filtfilt(b,a,lwing);
-    % rwing = filtfilt(b,a,rwing);
+  	% Filter wing angles
+    lwing = rad2deg(data.benifly.LWing);
+    rwing = rad2deg(data.benifly.RWing);
+    lwing = hampel(data.benifly.Time, lwing);
+    rwing = hampel(data.benifly.Time, rwing);
+	lwing = filtfilt(b,a,lwing);
+    rwing = filtfilt(b,a,rwing);
     
     % Get pattern, head, & body anlges
-    % pat = 3.75*PAT.pos;
+    pat = 3.75*PAT.pos;
+    body = data.body.bAngles;
+	head = data.head.head_data.angle;
     
-    % % Interpolate so all signals have the same times
-    % [b_pat, a_pat] = butter(3, 20 / (Fs/2), 'low');
-    % Reference = interp1(PAT.time_sync, pat, tintrp, 'nearest');
-    % %Reference = filtfilt(b_pat, a_pat, Reference);
-    % Reference = Reference - mean(Reference);
-    % Reference = 3.75*(round(Reference/3.75));
+    % Interpolate so all signals have the same times
+    [b_pat, a_pat] = butter(3, 20 / (Fs/2), 'low');
+    Reference = interp1(PAT.time_sync, pat, tintrp, 'nearest');
+    %Reference = filtfilt(b_pat, a_pat, Reference);
+    Reference = Reference - mean(Reference);
+    Reference = 3.75*(round(Reference/3.75));
     
     Body    = interp1(trig_time, body,  tintrp, 'pchip');
+    Body    = Body - mean(Body);
     Head    = interp1(trig_time, head,  tintrp, 'pchip');
-    % LWing   = interp1(trig_time, lwing, tintrp, 'pchip');
-    % RWing   = interp1(trig_time, rwing, tintrp, 'pchip');
-    % dWBA    = interp1(trig_time, lwing-rwing, tintrp, 'pchip');
+    Head    = filtfilt(b, a, Head);
+    LWing   = interp1(trig_time, lwing, tintrp, 'pchip');
+    RWing   = interp1(trig_time, rwing, tintrp, 'pchip');
+    dWBA    = interp1(trig_time, lwing-rwing, tintrp, 'pchip');
+    
+    % Detect & remove saccades
+    body_scd = saccade_v1(Body, tintrp, scd, false);
     
     % Store signals
-    % DATA.reference{n}       = singal_attributes(Reference, tintrp);
-    DATA.body{n}            = singal_attributes(Body, tintrp, 20, 0);
-    DATA.head{n}            = singal_attributes(Head, tintrp, 20, 2);
-    % DATA.dwba{n}            = singal_attributes(dWBA, tintrp, 20, 0);
-    % DATA.lwing{n}           = singal_attributes(LWing, tintrp, 20);
-    % DATA.rwing{n}           = singal_attributes(RWing, tintrp, 20);
+    n_detrend = 5;
+    DATA.body_saccade{n}    = body_scd;
+    DATA.reference{n}       = singal_attributes(Reference, tintrp);
+    DATA.body{n}            = singal_attributes(body_scd.shift.IntrpPosition, tintrp, [], 0);
+    DATA.head{n}            = singal_attributes(Head, tintrp, [], n_detrend);
+    DATA.dwba{n}            = singal_attributes(dWBA, tintrp, 20, n_detrend);
+    DATA.lwing{n}           = singal_attributes(LWing, tintrp, 20);
+    DATA.rwing{n}           = singal_attributes(RWing, tintrp, 20);
     
-    %Error                   = -DATA.body{n}.position - DATA.head{n}.position;
-	%DATA.error{n}           = singal_attributes(Error, tintrp);
+    Error                   = -DATA.body{n}.position - DATA.head{n}.position;
+	DATA.error{n}           = singal_attributes(Error, tintrp);
 
     % Debug plot
     if debug
@@ -152,26 +160,24 @@ for n = 1:N.file
         pause
     end
     
-    IOFreq = sort(FUNC{1}.All.Freq, 'ascend');
+    IOFreq = sort(FUNC{1}.FUNC{I{n,3}}.All.Freq, 'ascend');
     %REF = DATA.reference{n}.(clss);
     BODY = DATA.body{n}.(clss);
     HEAD = DATA.head{n}.(clss);
-    %ERROR = DATA.error{n}.(clss);
-    %dWBA = DATA.dwba{n}.position; % rerun for this
+    ERROR = DATA.error{n}.(clss);
+    dWBA = DATA.dwba{n}.position; % rerun for this
     %LWING = DATA.lwing{n}.(clss);
     %RWING = DATA.rwing{n}.(clss);
 
     %SYS_ref2_head_body = frf(tintrp, REF , IOFreq, false, BODY, HEAD);
 	SYS_body2_head = frf(tintrp, BODY, IOFreq, false, HEAD);
     %SYS_ref2_wing = frf(tintrp, DATA.reference{n}.position, IOFreq, false, dWBA);
-    %SYS_wing2_body = frf(tintrp, dWBA, IOFreq, false, BODY);
+    SYS_wing2_body = frf(tintrp, dWBA, IOFreq, false, BODY);
  	%SYS_err2_head_body = frf(tintrp, ERROR, IOFreq, false, BODY, HEAD);
     
-	%SYS_all = CatStructFields(2, SYS_body2_head, SYS_wing2_body);
+	SYS_all = CatStructFields(2, SYS_body2_head, SYS_wing2_body);
     
-    ALL{I.fly(n),I{n,3}}(end+1,1) = SYS_body2_head;
-    Body_All(:,n) = BODY - BODY(1);
-    Body_Freq(:,n) = DATA.body{n}.mag.velocity;
+    ALL{I.fly(n),I{n,3}}(end+1,1) = SYS_all;
 end
 
 %% Group Data
@@ -209,21 +215,38 @@ for v = 1:N{1,3}
     GRAND.all_trial(v) = structfun(@(x) system_stats(x,3), GRAND.all(v), 'UniformOutput', false);
 end
 
-%%
-close all
-subplot(2,1,1) ; cla ; hold on
-    plot(tintrp, Body_All, 'r', 'Color', [1 0 0 0.4], 'LineWidth', 0.75)
-    plot(Replay{1}.time, Replay{1}.pos.body(:,2) - Replay{1}.pos.body(1,2), 'k', 'LineWidth', 0.5)
-    plot(squeeze(GRAND.all(1).Time(:,1,:)), squeeze(GRAND.all(1).State(:,1,:)), 'b', 'LineWidth', 0.5)
-    
-subplot(2,1,2) ; cla ; hold on
-    plot(DATA.body{n}.Fv, Body_Freq, 'r', 'Color', [1 0 0 0.4], 'LineWidth', 0.75)
-    plot(Replay{1}.Fv, Replay{1}.freq.vel.body.mag(:,2), 'k', 'LineWidth', 0.5)
-    xlim([0 15])
-    set(gca, 'XScale', 'log')
+%% Plot
+n_trial = size(GRAND.all(1).refState(:,1,:), 3);
+cc = flipud(distinguishable_colors(n_trial));
 
+fig = figure (1); clf
+set(fig, 'Color', 'w', 'Units', 'inches')
+clear ax
 
+ax(1,1) = subplot(2,1,1); cla ; hold on
+body_replay_pos = FUNC{1}.replay.pos.body(:,2);
+plot(FUNC{1, 1}.replay.time, body_replay_pos, 'k', 'LineWidth', 2)
+for n = 1:n_trial
+    bPlot = GRAND.all(1).refState(:,1,n);
+    bPlot = bPlot - bPlot(1) + body_replay_pos(1);
+    plot(GRAND.all(1).Time(:,1,n), bPlot, 'LineWidth', 1, 'Color', cc(n,:))
+end
+xlabel('time (s)')
+ylabel('body (°)')
 
+ax(2,1) = subplot(2,1,2); cla ; hold on
+body_replay_mag = FUNC{1}.replay.freq.pos.body.mag(:,2);
+plot(FUNC{1, 1}.replay.Fv, body_replay_mag, 'k', 'LineWidth', 2)
+for n = 1:n_trial
+    bPlot = GRAND.all(1).refMag(:,1,n);
+    plot(GRAND.all(1).Fv(:,1,n), bPlot, 'LineWidth', 1, 'Color', cc(n,:))
+end
+xlabel('frequency (hz)')
+ylabel('body (°)')
+
+set(ax(2,1), 'XScale', 'log', 'XLim', [0.1 20])
+
+set(ax, 'Color', 'none', 'LineWidth', 1)
 
 %% Figure
 close all ; clc
@@ -241,11 +264,11 @@ ax(1) = subplot(4,1,1); cla ; hold on ; ylim([0 1])
     
 ax(2) = subplot(4,1,2); cla ; hold on ; %ylim([-200 100])
     yline(0, '--');
-    phs_lim = -50;
+    phs_lim = -100;
     phase_trial = rad2deg(squeeze(GRAND.all.IOPhaseDiff(:,pI,:)));
-    %phase_trial(phase_trial > phs_lim) = phase_trial(phase_trial > phs_lim) - 180;
+    phase_trial(phase_trial > phs_lim) = phase_trial(phase_trial > phs_lim) - 360;
     phase_mean = rad2deg(GRAND.fly_stats(1).circ_mean.IOPhaseDiff.circ_mean(:,pI));
-    %phase_mean(phase_mean > phs_lim) = phase_mean(phase_mean > phs_lim) - 360;
+    phase_mean(phase_mean > phs_lim) = phase_mean(phase_mean > phs_lim) - 360;
     
     plot(squeeze(GRAND.all.IOFv(:,1,:)), phase_trial, ...
         '.-', 'MarkerSize', 10, 'Color', [0.5 0.5 0.5 0.3], 'LineWidth', 0.25)
@@ -272,7 +295,7 @@ linkaxes(ax, 'x')
 %% SAVE
 disp('Saving...')
 savedir = 'E:\DATA\Magno_Data\Multibody';
-save(fullfile(savedir, [filename '_' datestr(now,'mm-dd-yyyy') '.mat']), ...
-    'FUNC', 'DATA', 'GRAND', 'FLY', 'Replay', 'D', 'I', 'U', 'N', 'T', '-v7.3')
+save(fullfile(savedir, [filename '_new3_' datestr(now,'mm-dd-yyyy') '.mat']), ...
+    'FUNC', 'DATA', 'GRAND', 'FLY', 'D', 'I', 'U', 'N', 'T', '-v7.3')
 disp('SAVING DONE')
 end
---
+++ tools/find_first_peak.m
@@ -0,0 +1,56 @@
+function [first_peak] = find_first_peak(signal, time, Fc, thresh, showplot)
+%% find_first_peak: get the start of the first peak in signal that meets criteria
+%
+%   INPUT:
+%     	signal        	: signal vector
+%     	time          	: time vector
+%       Fc              : cut off frequency [hz]
+%       thresh       	: velocity threshold
+%       showplot        : show plot (boolean)
+%       
+%   OUTPUT:
+%       first_peak          : signal to output from DAQ
+%
+
+if nargin < 5
+    showplot = true;
+end
+
+Fs = 1 / mean(diff(time));
+
+% Filter & take derivative of signal
+if ~isempty(Fc)
+    [b, a] = butter(5, Fc / (2*Fs), 'low');
+    signal_filt = filtfilt(b, a, signal);
+    signal_vel = central_diff(signal_filt, 1/Fs);
+    signal_vel = filtfilt(b, a, signal_vel);
+else
+    signal_filt = signal;
+    signal_vel = central_diff(signal_filt, 1/Fs);
+end
+
+% Find first peak location
+[~, locs] = findpeaks(signal_vel, 'MinPeakHeight', thresh);
+first_peak = locs(1);
+
+% Find where efirst peak starts
+start_thresh = 0.10*signal_vel(first_peak);
+search_signal = signal_vel(1:first_peak);
+search_time = time(1:first_peak);
+start_index = find(search_signal < start_thresh, 1, 'last');
+
+if showplot
+    subplot(2,1,1) ; cla ; hold on
+        plot(time, signal, 'k', 'LineWidth', 1)
+        plot(time(first_peak), signal(first_peak), '.r', 'MarkerSize', 20)
+        plot(search_time, signal(1:first_peak), 'b', 'LineWidth', 1)
+        plot(time(start_index), signal(start_index), '.g', 'MarkerSize', 20)
+    subplot(2,1,2) ; cla ; hold on
+        plot(time, signal_vel, 'k', 'LineWidth', 1)
+        plot(search_time, search_signal, 'b', 'LineWidth', 1)
+        plot(time(first_peak), signal_vel(first_peak), '.r', 'MarkerSize', 20)
+        plot(time(start_index), signal_vel(start_index), '.g', 'MarkerSize', 20)
+        yline(thresh, '--m');
+end
+
+end
